2e057e0f66faadca602f74346ded5a3f57491cc5
Nihan Zyan <nihanzyan38@gmail.com>
2022-04-30 17:09:14 +0600

    Header Added

--- src/App.js
+++ src/App.js
@@ -6,16 +6,20 @@ import Inventory from './component/Pages/Inventory/Inventory';
 import Blog from './component/Pages/Blog/Blog';
 import About from './component/Pages/About/About';
 import NotFound from './component/NotFound/NotFound';
+import Header from './component/Shared/Header/Header';
+import Login from './component/RequireAuth/Login/Login';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
        <Routes>
          <Route path='/' element={<Home></Home>}></Route>
          <Route path='/home' element={<Home></Home>}></Route>
          <Route path='/inventory' element={<Inventory></Inventory>}></Route>
          <Route path='/blog' element={<Blog></Blog>}></Route>
          <Route path='/about' element={<About></About>}></Route>
+         <Route path='/login' element={<Login></Login>}></Route>
          <Route path='/*' element={<NotFound></NotFound>}></Route>
        </Routes>
     </div>
--- src/component/Shared/Header/Header.js
+++ src/component/Shared/Header/Header.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../../images/sofa-mart-logo.png'
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className="header">
+                <div className="top-nav">
+                    <p><small>Need help? Call Us: + 880 295295
+                    </small></p>
+                </div>
+                <div className="container">
+                    <div className="logo">
+                        <img src={logo} alt="" />
+                    </div>
+                    <div className="bottom-nav">
+                        <div className="categories">
+                            <p>All Categories</p>
+                        </div>
+                        <div className="menu-bar">
+                            <Link className='menu' to={'/home'}>Home</Link>
+                            <Link className='menu' to={'/inventory'}>Inventory</Link>
+                            <Link className='menu' to={'/blog'}>Blog</Link>
+                            <Link className='menu' to={'/about'}>About</Link>
+                        </div>
+                        <div className="login">
+                            <Link className='menu login-menu' to={'/login'}>Login</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
